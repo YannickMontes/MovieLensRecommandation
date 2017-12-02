90c7a3b07a5eefa425547f16606060e2cdd9687d
MONTES Yannick <montes.yannick@gmail.com>
2017-12-02 12:48:09 -0500

    Fix mistakes

--- Code/User.cpp
+++ Code/User.cpp
@@ -1,13 +1,27 @@
 #include "User.h"
+#include <stdexcept>
 
 using namespace std;
 
-User::User()
+User::User(int id)
 {
-
+	this->id = id;
 }
 
 User::~User()
 {
 
+}
+
+bool User::hasRated(int movie)
+{
+	try
+	{
+		this->ratedMovies.at(movie);
+		return true;
+	}
+	catch(const out_of_range& oor)
+	{
+		return false;
+	}
 }
--- Code/User.h
+++ Code/User.h
@@ -1,21 +1,22 @@
 #pragma once
 #include <iostream>
 #include <map>
+#include <vector>
 
 class User
 {
 private:
 	int id;
 	std::map<int, int> ratedMovies;//Int id film, int rating
-	std::std::map<int, > unratedMovies;//Int id film, int guessed rating
+	std::map<int, int> unratedMovies;//Int id film, int guessed rating
 	std::vector<User> kClosestUsers;
 
 public:
-	User();
+	User(int);
 	~User();
 
 	int getId(){return this->id;};
 	bool hasRated(int movie);
 	int getRatingFor(int idFilm);
-	std::vector<int> getKClosestUsers(){return this->kClosestUsers;};
+	std::vector<User> getKClosestUsers(){return this->kClosestUsers;};
 };
